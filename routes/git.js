bb80eaf0d13a1d73992c1bef90d615c84d70e974
dipongkor <atish@bs-23.com>
2015-07-21 13:22:47 +0600

    log

--- routes/git.js
+++ routes/git.js
@@ -76,6 +76,7 @@ router.post("/getAllRepos", function (req, res, next) {
 });
 
 router.get("/webhook",function(req,res,next){
+    console.log("web hook fired");
    console.log(JSON.stringify(res));
    console.log(JSON.stringify(req));
    res.end(); 
