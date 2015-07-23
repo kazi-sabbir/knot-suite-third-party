4227babef142d518c914bd33bf12f72a12c5c07e
dipongkor <atish@bs-23.com>
2015-07-23 12:21:59 +0600

    signal test

--- modules/signal.js
+++ modules/signal.js
@@ -8,29 +8,40 @@ var signal = function () {
     function composeSignalContent(hookObj) {
         switch (hookObj.hookHeader["x-github-event"]) {
             case "issue_comment":
-                var content = "[" + hookObj.hookData.repository.full_name + "] " + "New comment on issue " + hookObj.hookData.issue.html_url + "\n"
+                var content = "[" + hookObj.hookData.repository.full_name + "] " + "New comment on issue " + "\n"
                     + "Commented by " + hookObj.hookData.comment.user.login + "\n"
                     + hookObj.hookData.comment.body;
-                return content;
+                return {
+                    content: content,
+                    ogDataUrl:  hookObj.hookData.issue.html_url
+                };
                 break;
             case "push":
                 var content = "[" + hookObj.hookData.repository.full_name + ": "+hookObj.hookData.ref.substr(hookObj.hookData.ref.lastIndexOf("/")+1, hookObj.hookData.ref)+"] " + "1 new commit by " + "\n"
-                + hookObj.hookData.commits[0].committer.name
+                + hookObj.hookData.commits[0].committer.name +"\n"
+                    +"Commit message: "+ hookObj.hookData.head_commit.message;
+                return {
+                    content: content,
+                    ogDataUrl:  hookObj.hookData.head_commit.url
+                };
+            break;
         }
     }
 
     function saveSignalFromGitWebHook(gitHook, hookObj) {
         console.log("Saving signal");
 
+        var signalData = composeSignalContent(hookObj);
+
         gitHook.orgList.forEach(function (org) {
             var data = {
                 accessToken: gitHook.knotAccessToken,
-                content: composeSignalContent(hookObj),
+                content: signalData.content,
                 spaceId: null,
                 rootId: null,
                 verb: null,
                 object: null,
-                activityType: "Composed-DashBoard-GitHub-Activity",
+                activityType: "Composed-DashBoard",
                 objectTags: {
                     objectTags: [],
                     hashTags: [],
@@ -51,21 +62,45 @@ var signal = function () {
                 }
             };
 
+
+
             request({
-                url: knotSettings.knotSuiteServiceUrl + "/api/signals/saveSignal",
+                url: knotSettings.knotSuiteServiceUrl + "/api/service/linkService/ogData",
                 method: "POST",
-                data: data,
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify(data)
-            }, function (err, res, body) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(res.body);
-                }
+                body: JSON.stringify({"url": signalData.ogDataUrl})
+            },function(err,res,body){
+               if(!res.code){
+
+                   var ogdataObject = {
+                       ogTitle: res.ogData.title,
+                       ogDescription: res.ogData.description,
+                       ogImage: res.ogData.images.length >0 ?  res.ogData.images[0] : "",
+                       isOgData: true,
+                       url: signalData.ogDataUrl
+                   }
+
+                   data.ogdataObject = ogdataObject;
+
+                   request({
+                       url: knotSettings.knotSuiteServiceUrl + "/api/signals/saveSignal",
+                       method: "POST",
+                       headers: {
+                           "Content-Type": "application/json"
+                       },
+                       body: JSON.stringify(data)
+                   }, function (err, res, body) {
+                       if (err) {
+                           console.log(err);
+                       } else {
+                           console.log(res.body);
+                       }
+                   });
+               }
             });
+
         });
     }
 };
